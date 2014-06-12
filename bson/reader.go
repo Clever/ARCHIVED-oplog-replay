2c8c9caee6251a1f128264cb1254c04070c2cdd3
Alex Zylman <alex.zylman@getclever.com>
2014-06-12 14:00:48 -0700

    bson: update comment

--- bson/reader.go
+++ bson/reader.go
@@ -9,10 +9,8 @@ import (
 
 func needMoreData() (int, []byte, error) { return 0, nil, nil }
 
-// mongodump outputs collections as binary files with all the documents appended together. We need to:
-//   1) Read 4 bytes (the size of the document, including these bytes)
-//   2) Read the size - 4 bytes (the rest of the document)
-//   3) Concatenate together these bytes to get the full document
+// mongodump outputs collections as binary files with all the documents appended together.
+// The first four bytes are the size of the full document, including the size bytes.
 func New(r io.Reader) *bufio.Scanner {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(func(data []byte, atEOF bool) (int, []byte, error) {

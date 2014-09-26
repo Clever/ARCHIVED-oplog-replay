a901c60afefecf1e6bb01805ddc8115d75444853
Kyle Vigen <kyle.vigen@clever.com>
2014-09-25 20:14:08 -0700

    Add replay for oplog-replay
    This is useful for reading files from s3, which fails ocassionally.

--- cmd/oplog-replay/main.go
+++ cmd/oplog-replay/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"time"
 
 	"github.com/Clever/oplog-replay/ratecontroller"
 	"github.com/Clever/oplog-replay/ratecontroller/fixed"
 	"github.com/Clever/oplog-replay/ratecontroller/relative"
 	"github.com/Clever/oplog-replay/replay"
 	"github.com/Clever/pathio"
+	"github.com/cenkalti/backoff"
 )
 
 func main() {
@@ -22,13 +25,37 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	input, err := pathio.Reader(*path)
+	input, err := readerWithRetry(*path)
 	if err != nil {
 		panic(err)
 	}
 	if err := replay.ReplayOplog(input, controller, *host); err != nil {
 		panic(err)
 	}
+}
+
+// readerWithRetry gets a reader from the path, retrying if necessary.
+func readerWithRetry(path string) (io.Reader, error) {
+	backoffObj := backoff.ExponentialBackOff{
+		InitialInterval:     5 * time.Second,
+		RandomizationFactor: backoff.DefaultRandomizationFactor,
+		Multiplier:          2,
+		MaxInterval:         30 * time.Second,
+		MaxElapsedTime:      2 * time.Minute,
+		Clock:               backoff.SystemClock,
+	}
+
+	var reader io.Reader
+	operation := func() error {
+		var err error
+		reader, err = pathio.Reader(path)
+		return err
+	}
+	if err := backoff.Retry(operation, &backoffObj); err != nil {
+		return nil, err
+	}
+	return reader, nil
+
 }
 
 func getControllerFromTypeAndSpeed(ratetype string, speed float64) (ratecontroller.Controller, error) {

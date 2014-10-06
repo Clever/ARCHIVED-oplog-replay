fdd7a3c97ed44ac55688ec9a809e2783380a6934
Kyle Vigen <kvigen@gmail.com>
2014-10-06 12:57:59 -0700

    Update run_as_worker.sh
    Update comment with better explanation

--- run_as_worker.sh
+++ run_as_worker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 gearcmd --name oplog-replay --cmd /usr/local/bin/oplogreplay --host $GEARMAN_HOST --port $GEARMAN_PORT &
 pid=$!
-# When we get a SIGTERM kill the child process and call wait. Note that we need wait both here
-# and the line below because of the semantics of the kill syscall. It seems that wait returns
-# the next time the state of the child process changes. Receiving a SIGTERM qualifies as a change
-# of state, so if we don't have a wait after `kill $pid` then we will complete the trap handler
-# and our call to `wait` below will complete. By calling `wait` after we kill the subprocess we wait
-# for the next process state change which is the actual process termination.
+# When we get a SIGTERM, forward it to the child process and call wait. Note that we wait both in here
+# and below (on line 10) because when bash gets a SIGTERM bash appears to cancel the currently running
+# command, call the trap handler, and then resume the script on the line after the line it was previously
+# running. That means that without waiting in the trap we could exit the script before gearcmd actually exits.
 trap "kill $pid && wait" SIGTERM SIGINT
 # Wait so that this script keeps running
 wait

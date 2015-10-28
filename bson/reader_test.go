6388e03ffe82bd2d2b81493c5d27cb27a45df426
Kyle Vigen <kyle.vigen@clever.com>
2015-10-28 11:15:13 -0700

    Fix map ordering issue
    Between go 1.2 and go 1.5 they made map iteration order random. This
    screwed up a bunch of these tests. I re-wrote them to have the right
    types and use reflect DeepEqual.

--- bson/reader_test.go
+++ bson/reader_test.go
@@ -1,8 +1,8 @@
 package bson
 
 import (
-	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"labix.org/v2/mgo/bson"
@@ -10,12 +10,12 @@ import (
 
 func TestParseBSON(t *testing.T) {
 	expected := []map[string]interface{}{
-		map[string]interface{}{"ts": 6021954198109683713, "h": 920013897904662416, "v": 2, "op": "c", "ns": "testdb.$cmd", "o": map[string]interface{}{"create": "test"}},
-		map[string]interface{}{"ts": 6021954253944258561, "h": -7024883673281943103, "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": "S\x92G}S\xa5\xb2\x9c\x16\xf84\xf1", "message": "insert test", "number": 1}},
-		map[string]interface{}{"ts": 6021954314073800705, "h": 8562537077519333892, "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": "S\x92G\x8bS\xa5\xb2\x9c\x16\xf84\xf2", "message": "update test", "number": 2}},
-		map[string]interface{}{"ts": 6021954326958702593, "h": 4976203120731500765, "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": "S\x92G\x95S\xa5\xb2\x9c\x16\xf84\xf3", "message": "delete test", "number": 3}},
-		map[string]interface{}{"ts": 6021954408563081217, "h": 5650666146636305048, "v": 2, "op": "u", "ns": "testdb.test", "o2": map[string]interface{}{"_id": "S\x92G\x8bS\xa5\xb2\x9c\x16\xf84\xf2"}, "o": map[string]interface{}{"_id": "S\x92G\x8bS\xa5\xb2\x9c\x16\xf84\xf2", "message": "update test", "number": 5}},
-		map[string]interface{}{"ts": 6021954451512754177, "h": -4953188477403348903, "v": 2, "op": "d", "ns": "testdb.test", "b": true, "o": map[string]interface{}{"_id": "S\x92G\x95S\xa5\xb2\x9c\x16\xf84\xf3"}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954198109683713), "h": int64(920013897904662416), "v": 2, "op": "c", "ns": "testdb.$cmd", "o": map[string]interface{}{"create": "test"}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954253944258561), "h": int64(-7024883673281943103), "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": bson.ObjectIdHex("5392477d53a5b29c16f834f1"), "message": "insert test", "number": 1}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954314073800705), "h": int64(8562537077519333892), "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": bson.ObjectIdHex("5392478b53a5b29c16f834f2"), "message": "update test", "number": 2}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954326958702593), "h": int64(4976203120731500765), "v": 2, "op": "i", "ns": "testdb.test", "o": map[string]interface{}{"_id": bson.ObjectIdHex("5392479553a5b29c16f834f3"), "message": "delete test", "number": 3}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954408563081217), "h": int64(5650666146636305048), "v": 2, "op": "u", "ns": "testdb.test", "o2": map[string]interface{}{"_id": bson.ObjectIdHex("5392478b53a5b29c16f834f2")}, "o": map[string]interface{}{"_id": bson.ObjectIdHex("5392478b53a5b29c16f834f2"), "message": "update test", "number": 5}},
+		map[string]interface{}{"ts": bson.MongoTimestamp(6021954451512754177), "h": int64(-4953188477403348903), "v": 2, "op": "d", "ns": "testdb.test", "b": true, "o": map[string]interface{}{"_id": bson.ObjectIdHex("5392479553a5b29c16f834f3")}},
 	}
 
 	f, err := os.Open("./testdata.bson")
@@ -32,7 +32,7 @@ func TestParseBSON(t *testing.T) {
 			t.Fatal("Got error in unmarshalling: ", err)
 		}
 
-		if fmt.Sprintf("%#v", op) != fmt.Sprintf("%#v", expected[nextOpIndex]) {
+		if !reflect.DeepEqual(op, expected[nextOpIndex]) {
 			t.Fatal("Op did not match expected!")
 		}
 		nextOpIndex++
@@ -47,14 +47,13 @@ func TestParseBSON(t *testing.T) {
 }
 
 func TestParseLargeBSON(t *testing.T) {
-	arraySize := 5000
-	largeArray := make([]interface{}, arraySize)
-	for i := 0; i < arraySize; i++ {
-		largeArray[i] = i
+	largeArray := make([]interface{}, 5000)
+	for i := 0; i < 5000; i++ {
+		largeArray[i] = float64(i)
 	}
 	expectedOp := map[string]interface{}{
-		"ts": 6048257058866724865, "h": -6825742652110581687, "v": 2, "op": "i", "ns": "testdb.testdb", "o": map[string]interface{}{
-			"_id": "S\xef\xb9\xc0g\xfd\x924\x8e\x828`",
+		"ts": bson.MongoTimestamp(6048257058866724865), "h": int64(-6825742652110581687), "v": 2, "op": "i", "ns": "testdb.testdb", "o": map[string]interface{}{
+			"_id": bson.ObjectIdHex("53efb9c067fd92348e823860"),
 			"val": largeArray}}
 
 	f, err := os.Open("./largetestdata.bson")
@@ -69,7 +68,7 @@ func TestParseLargeBSON(t *testing.T) {
 		if err := bson.Unmarshal(scanner.Bytes(), &op); err != nil {
 			t.Fatal("Error unmarshalling: ", err)
 		}
-		if fmt.Sprintf("%#v", op) == fmt.Sprintf("%#v", expectedOp) {
+		if reflect.DeepEqual(op, expectedOp) {
 			foundExpectedOp = true
 		}
 	}
